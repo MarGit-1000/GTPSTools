9b9f219a695788dd4156d5a2449ffd501147a80a
MarGit-1000 <ikramazzamy129@gmail.com>
2025-06-11 13:06:05 +0800

    Update api.js

--- api/api.js
+++ api/api.js
@@ -1,6 +1,6 @@
 import express from "express";
 import cors from "cors";
-import { encodeItems } from "encoder.js";
+import { encodeItems } from "./encoder.js";
 
 const app = express();
 app.use(cors());
