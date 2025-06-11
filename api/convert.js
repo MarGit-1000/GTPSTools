ac9587405b72f8fee7a93023ca57039554081796
MarGit-1000 <ikramazzamy129@gmail.com>
2025-06-11 12:16:58 +0800

    Update convert.js

--- api/convert.js
+++ api/convert.js
@@ -1,1 +1,12 @@
-get api
+import { convertItems } from "../GTPSTools/items/items.js";
+
+export default function handler(req, res) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  if (req.method === "POST") {
+    const data = req.body;
+    const converted = convertItems(data);
+    res.status(200).json({ success: true, converted });
+  } else {
+    res.status(405).json({ error: "Method not allowed" });
+  }
+}
