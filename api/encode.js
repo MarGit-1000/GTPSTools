bee0017115cac49dd61160b5cc70946acb6dbade
MarGit-1000 <ikramazzamy129@gmail.com>
2025-06-11 12:49:43 +0800

    Update encode.js

--- api/encode.js
+++ api/encode.js
@@ -1,4 +1,4 @@
-import { encodeItems } from "../items/encoder.js";
+import { encodeItems } from "encoder.js";
 
 export default async function handler(req, res) {
   if (req.method !== "POST") return res.status(405).json({ error: "Method not allowed" });
